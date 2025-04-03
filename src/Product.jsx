b23de17540f7757e3154ef7deedfb88c7e923298
raj140104 <riteshraj140104@gmail.com>
2025-04-03 14:31:26 +0530

    Rendering Array

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,14 @@
 import "./Product.css";
 
 
-function Product({ title, price = 1 }) {
-
+function Product({ title, price, features }) {
+    const list = features?.map((feature, index) => <li key={index}>{feature}</li>);
     return (
         <div className="Product">
             <h3>{title}</h3>
             <h4>Price:{price}</h4>
+            <p>{list}</p>
+
         </div>
     )
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -2,11 +2,14 @@ import Product from "./Product";
 
 
 function ProductTab() {
+
+    let options2 = ["hi-tech", "durable", "fast"];
+    //let options2 = { a: "high-tech", b: "durable", c: "fast" };
     return (
         <>
-            <Product title="Phone" price={10000} />
-            <Product title="Tab" rice={5000} />
-            <Product title="Mobile" />
+            <Product title="Phone" price={10000} features={options2} />
+            <Product title="Tab" price={5000} />
+            <Product title="Mobile" price={1000} />
 
 
         </>
