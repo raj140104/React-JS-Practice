803900895b1dd4dc37696402bd77025e325045cc
raj140104 <riteshraj140104@gmail.com>
2025-04-06 17:05:06 +0530

    Understanding of States and Created Like button

--- src/LikeButton.jsx
+++ src/LikeButton.jsx
@@ -1,30 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function LikeButton() {
-    let [isLiked, setIsLiked] = useState(false);
-    let [clicks, setCount] = useState(0);
 
-    let counting = () => {
-        setCount(clicks + 1);
-    }
+
+    let [isLiked, setIsLiked] = useState(false);
+
+
     let toggleLike = () => {
         setIsLiked(!isLiked);
-        setCount(clicks + 1);
     }
 
-    let styles = { color: "red" }
+    let styles = { color: "red" };
     return (
         <div>
             <p onClick={toggleLike}>
                 {
-                    isLiked ? (<i class="fa-sharp fa-solid fa-heart" style={styles}></i>) : (<i className="fa-regular fa-heart" style={styles}></i>)
+                    isLiked ? (< i className="fa-solid fa-heart" style={styles}></i>) : (<i className="fa-regular fa-heart"></i>)
                 }
 
             </p>
-            <p>
-                clicks = {clicks}
-            </p>
-
         </div>
     )
 }
